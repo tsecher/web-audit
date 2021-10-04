366eeb008bdc76c335fffc3f7ba96865906647a5
Thomas SECHER <thomas.secher@consertotech.pro>
2021-10-04 13:55:58 +0200

    Meilleur gestion des prompts

--- src/seo_audit/audit.js
+++ src/seo_audit/audit.js
@@ -7,45 +7,58 @@ let url = parameters[2];
 
 // Prompts
 const prompts = require('prompts');
+const {replaceIcuMessageInstanceIds} = require("lighthouse/lighthouse-core/lib/i18n/i18n");
 (async () => {
-  const response = await prompts([{
-    type: 'text',
-    name: 'url',
-    message: 'Domain to audit ?',
-    initial: url,
-  },
-  {
-      type: 'multiselect',
-      name: 'types',
-      message: "Quel type d'audit ?",
-      choices: [
-        { title: 'SEO', value: 'seo' },
-        { title: 'Lighthouse', value: 'lighthouse' },
-        { title: 'Ecoindex', value: 'ecoindex' }
-      ],
-  }]);
+    const response = await prompts([
+        {
+            type: 'text',
+            name: 'url',
+            message: 'Domain to audit ?',
+            initial: url,
+        },
+        {
+            type: 'multiselect',
+            name: 'types',
+            message: "Quel type d'audit ?",
+            choices: [
+                {title: 'SEO', value: 'seo', selected: true},
+                {title: 'Lighthouse', value: 'lighthouse', selected: true},
+                {title: 'Ecoindex', value: 'ecoindex', selected: true}
+            ],
+        },
+        {
+            type: 'toggle',
+            name: 'selection',
+            message: 'Ajouter automatiquement les nouvelles urls à la sélection ?',
+            initial: true,
+            active: 'oui',
+            inactive: 'non'
+        },
+    ]);
 
-  // Prepare data.
-  url - response.url
+    // Prepare data.
+    url = response.url
 
-  // Lancement des audits.
-  // SEO
-  if( response.types.indexOf('seo') > -1 ){
-    console.log('=========================');
-    console.log('=========================');
-    console.log(' SITE AUDIT');
-    const SiteAuditor = require("./src/site-auditor.js")
-    const seo = new SiteAuditor(url, response.types.length > 1 ? 1 : 0);
-    seo.run().then(()=>{
-        
-    })
-  }
+    // Lancement des audits.
+    // SEO
+    if (response.types.indexOf('seo') > -1) {
+        console.log('=========================');
+        console.log('=========================');
+        console.log(' SITE AUDIT');
+        const SiteAuditor = require("./src/site-auditor.js")
+        const seo = new SiteAuditor(url, response.selection? 1 : 0);
+        seo.run().then(() => {
+            deepAudit(response)
+        })
+    } else {
+        deepAudit(response)
+    }
 
 })();
 
-function deepAudit(){
+function deepAudit(response) {
     // Lighthouse
-    if( response.types.indexOf('lighthouse') > -1 ){
+    if (response.types.indexOf('lighthouse') > -1) {
         console.log('=========================');
         console.log('=========================');
         console.log(' LIGHTHOUSE');
@@ -55,7 +68,7 @@ function deepAudit(){
     }
 
     // Ecoindex.
-    if( response.types.indexOf('ecoindex') > -1 ){
+    if (response.types.indexOf('ecoindex') > -1) {
         console.log('=========================');
         console.log('=========================');
         console.log('ECOINDEX');
