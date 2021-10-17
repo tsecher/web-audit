c87fa81e72308deeca6d0c6f628c64a168e3d6a6
Thomas SECHER <thomas.secher@consertotech.pro>
2021-10-17 13:15:30 +0200

    Add parameters tools

--- src/seo_audit/audit.js
+++ src/seo_audit/audit.js
@@ -1,46 +1,76 @@
 #!/usr/bin/env node
 
+const parameters = require('args-parser')(process.argv);
 
-const parameters = process.argv;
-let url = parameters[2];
+// Initialisation des settings.
+const settings = [
+    {
+        type: 'text',
+        name: 'url',
+        message: 'Domain to audit ?',
+    },
+    {
+        type: 'multiselect',
+        name: 'types',
+        message: "Quel type d'audit ?",
+        choices: [
+            {title: 'SEO', value: 'seo', selected: true},
+            {title: 'Lighthouse', value: 'lighthouse', selected: true},
+            {title: 'Ecoindex', value: 'ecoindex', selected: true}
+        ],
+    },
+    {
+        type: 'toggle',
+        name: 'selection',
+        message: 'Ajouter automatiquement les nouvelles urls à la sélection ?',
+        initial: true,
+        active: 'oui',
+        inactive: 'non'
+    },
+]
 
+// Initialisation des settings.
+settings.forEach(setting => {
+    const id = setting.name;
+    if (parameters[id]) {
+        let value = setting.initial;
+        switch (setting.type) {
+            case 'toggle':
+                value = [1, '1', 'true', true].indexOf(parameters[id]) > -1;
+                break;
+            case 'multiselect':
+                value = parameters[id].split(',');
+                setting.choices.forEach(choice => {
+                    choice.selected = value.indexOf(choice.value) > -1;
+                })
+                break;
+            default:
+                value = parameters[id];
+        }
+        parameters[id] = value;
+        setting.initial = value;
+    } else {
+        if (parameters['y']) {
+            switch (setting.type) {
+                case 'multiselect':
+                    parameters[id] = setting.choices
+                        .filter(choice => choice.selected)
+                        .map(choice => choice.value)
+                    break;
+                default:
+                    parameters[id] = setting.initial
+            }
+        }
+    }
+})
 
 // Prompts
 const prompts = require('prompts');
-const {replaceIcuMessageInstanceIds} = require("lighthouse/lighthouse-core/lib/i18n/i18n");
-const SiteAuditor = require("./src/site-auditor.js");
-const LighthouseAuditor = require("./src/lighthouse-auditor");
-const EcoindexAuditor = require("./src/ecoindex-auditor");
 (async () => {
-    const response = await prompts([
-        {
-            type: 'text',
-            name: 'url',
-            message: 'Domain to audit ?',
-            initial: url,
-        },
-        {
-            type: 'multiselect',
-            name: 'types',
-            message: "Quel type d'audit ?",
-            choices: [
-                {title: 'SEO', value: 'seo', selected: true},
-                {title: 'Lighthouse', value: 'lighthouse', selected: true},
-                {title: 'Ecoindex', value: 'ecoindex', selected: true}
-            ],
-        },
-        {
-            type: 'toggle',
-            name: 'selection',
-            message: 'Ajouter automatiquement les nouvelles urls à la sélection ?',
-            initial: true,
-            active: 'oui',
-            inactive: 'non'
-        },
-    ]);
-
-    // Prepare data.
-    url = response.url
+    let response = parameters;
+    if (!parameters.y) {
+        response = await prompts(settings)
+    }
 
     const listAudits = {
         'seo': () => {
@@ -48,7 +78,7 @@ const EcoindexAuditor = require("./src/ecoindex-auditor");
             console.log('=========================');
             console.log(' SITE AUDIT');
             const SiteAuditor = require("./src/site-auditor.js")
-            const seo = new SiteAuditor(url, response.selection ? 1 : 0);
+            const seo = new SiteAuditor(response.url, response.selection ? 1 : 0);
             seo.run().then(() => {
                 nextAudit()
             })
@@ -58,9 +88,8 @@ const EcoindexAuditor = require("./src/ecoindex-auditor");
             console.log('=========================');
             console.log(' LIGHTHOUSE');
             const LighthouseAuditor = require("./src/lighthouse-auditor")
-            const lighthouse = new LighthouseAuditor(url, '../selection', 'lighthouse');
+            const lighthouse = new LighthouseAuditor(response.url, '../selection', 'lighthouse');
             lighthouse.run().then(() => {
-                console.log('erjtjtjerotjeotjeoritjoj')
                 nextAudit()
             })
         },
@@ -69,7 +98,7 @@ const EcoindexAuditor = require("./src/ecoindex-auditor");
             console.log('=========================');
             console.log('ECOINDEX');
             const EcoindexAuditor = require("./src/ecoindex-auditor")
-            const ecoindex = new EcoindexAuditor(url, '../selection', 'ecoindex');
+            const ecoindex = new EcoindexAuditor(response.url, '../selection', 'ecoindex');
             ecoindex.run().then(() => {
                 nextAudit()
             })
@@ -83,16 +112,11 @@ const EcoindexAuditor = require("./src/ecoindex-auditor");
         const nextAuditIndex = audits.reverse().pop();
         response.types.reverse();
 
-        console.log('=====================================', nextAuditIndex)
-        if( typeof listAudits[nextAuditIndex] === 'function'){
+        if (typeof listAudits[nextAuditIndex] === 'function') {
             listAudits[nextAuditIndex]();
-        }
-        else{
+        } else {
         }
     }
 
-
     nextAudit();
-
-
 })();
