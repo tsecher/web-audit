dbf3c325b19bdf84b1d0fac6392e37a24ada5f0f
Thomas SECHER <thomas.secher@kleegroup.com>
2022-05-14 16:44:03 +0200

    Web audit for windows

--- src/seo_audit/src/tools/logger.js
+++ src/seo_audit/src/tools/logger.js
@@ -13,7 +13,7 @@ class Logger {
 	 * Initialise le rep d'analise.
 	 */
 	initDir() {
-		const analyseDir  = (process.env.PWD || process.env.INIT_CWD) + '/seo_analyse/';
+		const analyseDir  = (process.cwd()) + '/seo_analyse/';
 
 		if (!fs.existsSync(analyseDir)) {
 			fs.mkdirSync(analyseDir);
